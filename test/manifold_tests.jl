6c8cbbd89bbd62359b8bb2010d338e0db16789ee
ChrisRackauckas <chrisrackauckas@gmail.com>
2017-07-11 17:01:28 -0700

    make manifold handle n-d

--- test/manifold_tests.jl
+++ test/manifold_tests.jl
@@ -1,15 +1,17 @@
 using OrdinaryDiffEq, Base.Test, DiffEqBase, DiffEqCallbacks
 
-u0 = ones(2)
+u0 = ones(2,2)
 f = function (t,u,du)
-  du[1] = u[2]
-  du[2] = -u[1]
+  du[1,:] = u[2,:]
+  du[2,:] = -u[1,:]
 end
 prob = ODEProblem(f,u0,(0.0,100.0))
 
 function g(u,resid)
   resid[1] = u[2]^2 + u[1]^2 - 2
-  resid[2] = 0
+  resid[2] = u[3]^2 + u[4]^2 - 2
+  resid[3] = 0
+  resid[4] = 0
 end
 
 cb = ManifoldProjection(g)
