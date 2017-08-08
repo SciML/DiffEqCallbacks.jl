f2b18fdbfd34432955d903bad07f17b281a7f2e5
David Widmann <dev+git@devmotion.de>
2017-08-08 15:28:27 +0200

    Always take absolute values

--- src/autoabstol.jl
+++ src/autoabstol.jl
@@ -4,9 +4,9 @@ end
 # Now make `affect!` for this:
 function (p::AutoAbstolAffect)(integrator)
   if typeof(p.curmax) <: AbstractArray
-    p.curmax .= max.(p.curmax,integrator.u)
+    @. p.curmax = max(p.curmax,abs(integrator.u))
   else
-    p.curmax = max(p.curmax,maximum(integrator.u))
+    p.curmax = max(p.curmax,maximum(abs.(integrator.u)))
   end
 
   if typeof(integrator.opts.abstol) <: AbstractArray
@@ -14,7 +14,7 @@ function (p::AutoAbstolAffect)(integrator)
   else
     integrator.opts.abstol = p.curmax .* integrator.opts.reltol
   end
-  
+
   u_modified!(integrator,false)
 end
 
@@ -25,7 +25,7 @@ function AutoAbstol_initialize(cb,t,u,integrator)
 end
 
 function AutoAbstol(save=true;init_curmax=0.0)
-  affect! = AutoAbstolAffect(init_curmax)
+  affect! = AutoAbstolAffect(abs.(init_curmax))
   condtion = (t,u,integrator) -> true
   save_positions = (save,false)
   DiscreteCallback(condtion,affect!;
