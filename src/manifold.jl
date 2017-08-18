65ede6829256a669e2d77f516375511719be6bac
David Widmann <dev+git@devmotion.de>
2017-08-18 18:39:25 +0200

    Add NLsolve options and non-autonomous functions to ManifoldProjection

--- src/manifold.jl
+++ src/manifold.jl
@@ -24,16 +24,18 @@ function autodiff_setup{CS}(f!, initial_x,chunk_size::Type{Val{CS}})
         DiffBase.value(jac_res)
     end
 
-    return DifferentiableMultivariateFunction(f!, g!, fg!)
+    return DifferentiableMultivariateFunction((x,resid)->f!(reshape(x,size(initial_x)...),
+                                                            resid),
+                                              g!, fg!)
 end
 
 function non_autodiff_setup(f!, initial_x)
-  DifferentiableMultivariateFunction((resid,x)->f!(resid,reshape(x,size(initial_x)...)))
+  DifferentiableMultivariateFunction((x,resid)->f!(reshape(x,size(initial_x)...), resid))
 end
 
 immutable NLSOLVEJL_SETUP{CS,AD} end
 Base.@pure NLSOLVEJL_SETUP(;chunk_size=0,autodiff=true) = NLSOLVEJL_SETUP{chunk_size,autodiff}()
-(p::NLSOLVEJL_SETUP)(f,u0) = (res=NLsolve.nlsolve(f,u0); res.zero)
+(p::NLSOLVEJL_SETUP)(f, u0; kwargs...) = (res=NLsolve.nlsolve(f, u0; kwargs...); res.zero)
 function (p::NLSOLVEJL_SETUP{CS,AD}){CS,AD}(::Type{Val{:init}},f,u0_prototype)
   if AD
     return non_autodiff_setup(f,u0_prototype)
@@ -47,28 +49,52 @@ get_chunksize{CS,AD}(x::NLSOLVEJL_SETUP{CS,AD}) = CS
 
 #########################
 
-type ManifoldProjection{NL}
+# wrapper for non-autonomous functions
+mutable struct NonAutonomousFunction{F}
+  f::F
+  t
+end
+(p::NonAutonomousFunction)(u, res) = p.f(p.t, u, res)
+
+mutable struct ManifoldProjection{autonomous,F,NL,O}
+  g::F
   nl_rhs
   nlsolve::NL
+  nlopts::O
+
+  function ManifoldProjection{autonomous}(g, nlsolve, nlopts) where {autonomous}
+    # replace residual function if it is time-dependent
+    # since NLsolve only accepts functions with two arguments
+    if !autonomous
+      g = NonAutonomousFunction(g, 0)
+    end
+
+    new{autonomous,typeof(g),typeof(nlsolve),typeof(nlopts)}(g, g, nlsolve, nlopts)
+  end
 end
+
 # Now make `affect!` for this:
-function (p::ManifoldProjection)(integrator)
-  nlres = reshape(p.nlsolve(p.nl_rhs,vec(integrator.u)),size(integrator.u)...)::typeof(integrator.u)
+function (p::ManifoldProjection{autonomous,NL})(integrator) where {autonomous,NL}
+  # update current time if residual function is time-dependent
+  if !autonomous
+    p.g.t = integrator.t
+  end
+
+  nlres = reshape(p.nlsolve(p.nl_rhs, vec(integrator.u); p.nlopts...),
+                  size(integrator.u)...)::typeof(integrator.u)
   integrator.u .= nlres
 end
 
 function Manifold_initialize(cb,t,u,integrator)
-  cb.affect!.nl_rhs = cb.affect!.nlsolve(
-                      Val{:init},
-                      cb.affect!.nl_rhs,
-                      integrator.u)
+  cb.affect!.nl_rhs = cb.affect!.nlsolve(Val{:init}, cb.affect!.g, u)
 end
 
-function ManifoldProjection(g;nlsolve=NLSOLVEJL_SETUP(),save=true)
-  affect! = ManifoldProjection(g,nlsolve)
+function ManifoldProjection(g; nlsolve=NLSOLVEJL_SETUP(), save=true,
+                            autonomous=numargs(g)==2, nlopts=Dict{Symbol,Any}())
+  affect! = ManifoldProjection{autonomous}(g, nlsolve, nlopts)
   condtion = (t,u,integrator) -> true
   save_positions = (false,save)
-  DiscreteCallback(condtion,affect!;
+  DiscreteCallback(condtion, affect!;
                    initialize = Manifold_initialize,
                    save_positions=save_positions)
 end
