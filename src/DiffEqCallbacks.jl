22b061d4e402ef3a91a3ddaf75b617da6f4f9570
Hendrik Ranocha <h.ranocha@tu-bs.de>
2017-09-29 08:07:31 +0200

    adapted to ForwardDiff.jl v0.6.0

--- src/DiffEqCallbacks.jl
+++ src/DiffEqCallbacks.jl
@@ -3,7 +3,7 @@ __precompile__()
 module DiffEqCallbacks
 
   using DiffEqBase, NLsolve, ForwardDiff
-  import DiffBase
+  import DiffResults
 
   import OrdinaryDiffEq: fix_dt_at_bounds!, modify_dt_for_tstops!
 
