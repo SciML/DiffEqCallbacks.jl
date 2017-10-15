b5c7a246b04d58666119ff42dea95ffa0368bd76
Twan Koolen <koolen.twan@gmail.com>
2017-10-14 23:56:06 -0400

    Make things work (in theory) for negative tdir.

--- src/periodic.jl
+++ src/periodic.jl
@@ -1,6 +1,4 @@
 function PeriodicCallback(f, Δt::Number; initialize = DiffEqBase.INITIALIZE_DEFAULT, kwargs...)
-    @assert Δt > 0
-
     # Value of `t` at which `f` should be called next:
     tnext = Ref(typemax(Δt))
     condition = (t, u, integrator) -> t == tnext[]
@@ -13,7 +11,7 @@ function PeriodicCallback(f, Δt::Number; initialize = DiffEqBase.INITIALIZE_DEF
         tnew = tnext[] + Δt
         tstops = integrator.opts.tstops
         for i in length(tstops) : -1 : 1 # reverse iterate to encounter large elements earlier
-            if tstops.valtree[i] > tnew # TODO: relying on implementation detail
+            if DataStructures.compare(tstops.comparer, tnew, tstops.valtree[i]) # TODO: relying on implementation details
                 tnext[] = tnew
                 add_tstop!(integrator, tnew)
                 break
@@ -23,6 +21,7 @@ function PeriodicCallback(f, Δt::Number; initialize = DiffEqBase.INITIALIZE_DEF
 
     # Initialization: first call to `f` should be *before* any time steps have been taken:
     initialize_periodic = function (c, t, u, integrator)
+        @assert integrator.tdir == sign(Δt)
         initialize(c, t, u, integrator)
         tnext[] = t
         affect!(integrator)
