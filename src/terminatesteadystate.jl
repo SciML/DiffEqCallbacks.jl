06909c24488d927f6c532f4be2b610d049540b1b
Alejandro Morales Sierra <morales.s.alejandro@gmail.com>
2018-02-17 15:27:55 +0100

    Improvements to `allDerivPass`
    Use cache for `du` when possible.
    Use iterator inside `any` with cycling zip
    Fuse both conditions such that only one needs to be met
    Add test for out-of-place and in-place models
    No need to pass `t` to test function.

--- src/terminatesteadystate.jl
+++ src/terminatesteadystate.jl
@@ -1,12 +1,21 @@
 
+# Check if a cache for du can be used inside allDerivPass
+Base.@pure usecache(z) = true
+Base.@pure usecache(z::Number) = false
+Base.@pure usecache(z::StaticArrays.SArray) = false
+
 # Default test function
 # Terminate when all derivatives fall below a threshold or
-#   when derivatives are small than a fraction of state
-function allDerivPass(integrator, t, abstol, reltol)
-    du = integrator(t, Val{1})
-    any(du .> abstol) &&  (return false)
-    u = integrator(t)
-    any(du .> reltol.*u) &&  (return false)
+#   when derivatives are smaller than a fraction of state
+function allDerivPass(integrator, abstol, reltol)
+    if usecache(integrator.u)
+        du = first(get_tmp_cache(integrator))
+        DiffEqBase.get_du!(du, integrator)
+    else
+        du = get_du(integrator)
+    end
+    any(abs(d) > abstol && abs(d) > reltol*abs(u) for (d,abstol, reltol, u) =
+           zip(du, cycle(abstol), cycle(reltol), integrator.u)) && (return false)
     return true
 end
 
@@ -14,7 +23,7 @@ end
 # test function must take integrator, time, followed by absolute
 #   and relative tolerance and return true/false
 function TerminateSteadyState(abstol = 1e-8, reltol = 1e-6, test = allDerivPass)
-    condition = (u, t, integrator) -> test(integrator, t, abstol, reltol)
+    condition = (u, t, integrator) -> test(integrator, abstol, reltol)
     affect! = (integrator) -> terminate!(integrator)
     DiscreteCallback(condition, affect!)
 end
