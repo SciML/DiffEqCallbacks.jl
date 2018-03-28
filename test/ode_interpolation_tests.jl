a4cc3891f4a67dc3a0f2e47ff2b22a1923c9a837
Niklas Korsbo <niklas.korsbo@slcu.cam.ac.uk>
2018-03-28 11:01:48 +0100

    Test against analytical solution for entire timespan.

--- test/ode_interpolation_tests.jl
+++ test/ode_interpolation_tests.jl
@@ -1,11 +1,14 @@
 using OrdinaryDiffEq, Base.Test, DiffEqBase, DiffEqCallbacks
 
-function testfunction(du, u, p, t)
+function test_ode(du, u, p, t)
     du[1] = p[1] - (1 - p[1])*u[1]
     return nothing
 end
 
-testtimes = linspace(0.,4.99,1000)
+test_solution(t) = t <= 5 ? t : 5. * e^(-(t-5))
+
+tspan = (0.,10.)
+testtimes = linspace(tspan..., 1001)
 condition(u, t, i) = t - 5
 affect!(i) = i.p[1] = abs(1 - i.p[1])
 cb = ContinuousCallback(condition, affect!)
@@ -18,10 +21,10 @@ passed = fill(false, length(algs))
 
 for (i, alg) in enumerate(algs)
     display("testing $alg")
-    prob = ODEProblem(testfunction, [0.], (0.,10.), [1.])
+    prob = ODEProblem(test_ode, [0.], tspan, [1.])
     sol = solve(prob, alg(); callback=cb)
 
-    passed[i] = all(isapprox(sol(t)[1], t; atol=0.05) for t in testtimes)
+    passed[i] = all(isapprox(sol(t)[1], test_solution(t); atol=0.05) for t in testtimes)
     @test passed[i]
 end
 
