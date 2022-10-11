d397e3a59f58eece1149e66475cd8d1c18949ca3
Christopher Rackauckas <accounts@chrisrackauckas.com>
2022-10-11 22:36:05 +0200

    Update make.jl

--- docs/make.jl
+++ docs/make.jl
@@ -1,5 +1,9 @@
 using Documenter, DiffEqCallbacks
 
+# Make sure that plots don't throw a bunch of warnings / errors!
+ENV["GKSwstype"] = "100"
+using Plots
+
 include("pages.jl")
 
 makedocs(sitename = "DiffEqCallbacks.jl",
