546db0dd453ba415fe8e64b5edaa08b74e092fd0
David Widmann <devmotion@users.noreply.github.com>
2024-11-28 17:15:43 +0100

    Revert convergence criterion

--- src/terminatesteadystate.jl
+++ src/terminatesteadystate.jl
@@ -21,17 +21,12 @@ function allDerivPass(integrator, abstol, reltol, min_t)
     end
 
     if integrator.u isa Array
-        any(abs(d) > abstol || abs(d) > reltol * abs(u)
-        for (d, abstol, reltol, u) in zip(testval, Iterators.cycle(abstol),
-            Iterators.cycle(reltol), integrator.u)) &&
-            (return false)
+        return all(abs(d) <= max(abstol, reltol * abs(u))
+            for (d, abstol, reltol, u) in zip(testval, Iterators.cycle(abstol),
+            Iterators.cycle(reltol), integrator.u))
     else
-        any((abs.(testval) .> abstol) .||
-            (abs.(testval) .> reltol .* abs.(integrator.u))) &&
-            (return false)
+        return all(abs.(testval) .<= max.(abstol, reltol .* abs.(integrator.u)))
     end
-
-    return true
 end
 
 struct WrappedTest{T, A, R, M}
@@ -60,7 +55,7 @@ the [Steady State Solvers](https://docs.sciml.ai/DiffEqDocs/stable/solvers/stead
     These tolerances may be specified as scalars or as arrays of the same length
     as the states of the problem.
   - `test` represents the function that evaluates the condition for termination. The default
-    condition is that all derivatives should become smaller than `abstol` and the states times
+    condition is that all derivatives should become smaller than `abstol` or the states times
     `reltol`. The user can pass any other function to implement a different termination condition.
     Such function should take four arguments: `integrator`, `abstol`, `reltol`, and `min_t`.
   - `wrap_test` can be set to `Val(false)`, in which case `test` must have the definition
