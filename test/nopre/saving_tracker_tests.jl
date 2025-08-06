1f9b0770a925c2dc9e5539cbc8cd0bcf84a16e1e
ChrisRackauckas <accounts@chrisrackauckas.com>
2025-08-06 06:52:43 -0400

    Separate sensitivity analysis tests into NoPre test group
    - Created NoPre test group for tests requiring SciMLSensitivity, Zygote, and Tracker
    - Moved sensitivity-specific portions from integrating_tests.jl to nopre/integrating_sensitivity_tests.jl
    - Moved sensitivity-specific portions from integrating_sum_tests.jl to nopre/integrating_sum_sensitivity_tests.jl
    - Moved Tracker tests from saving_tests.jl to nopre/saving_tracker_tests.jl
    - Created separate Project.toml for NoPre tests with required dependencies
    - Updated main Project.toml to remove sensitivity-related dependencies from base tests
    - Kept ForwardDiff and QuadGK in base tests as they're used by non-sensitivity tests
    - Updated runtests.jl to conditionally run NoPre tests with separate environment
    
    This follows the same pattern as LinearSolve.jl for separating tests with heavy dependencies.

---
+++ test/nopre/saving_tracker_tests.jl
@@ -0,0 +1,39 @@
+using OrdinaryDiffEqTsit5, DiffEqCallbacks
+using SciMLSensitivity, Tracker
+using Test
+
+# Tracker with Saving Callback
+## This is pretty much a hack. It has been merged into DistributionsAD master
+Base.prevfloat(r::Tracker.TrackedReal) = Tracker.track(prevfloat, r)
+Tracker.@grad function prevfloat(r::Real)
+    prevfloat(Tracker.data(r)), Δ -> (Δ,)
+end
+Base.nextfloat(r::Tracker.TrackedReal) = Tracker.track(nextfloat, r)
+Tracker.@grad function nextfloat(r::Real)
+    nextfloat(Tracker.data(r)), Δ -> (Δ,)
+end
+
+function rober(u, p::TrackedArray, t)
+    y₁, y₂, y₃ = u
+    k₁, k₂, k₃ = p
+    return Tracker.collect([-k₁ * y₁ + k₃ * y₂ * y₃,
+        k₁ * y₁ - k₂ * y₂^2 - k₃ * y₂ * y₃,
+        k₂ * y₂^2])
+end
+
+p = TrackedArray([1.9f0, 1.0f0, 3.0f0])
+u0 = TrackedArray([1.0f0, 0.0f0, 0.0f0])
+tspan = TrackedArray([0.0f0, 1.0f0])
+prob = ODEProblem{false}(rober, u0, tspan, p)
+saved_values = SavedValues(eltype(tspan), eltype(p))
+cb = SavingCallback((u, t, integrator) -> integrator.EEst * integrator.dt, saved_values)
+
+@test !all(iszero.(Tracker.gradient(
+    p -> begin
+        solve(remake(prob, u0 = u0, p = p, tspan = tspan),
+            Tsit5(),
+            sensealg = SensitivityADPassThrough(),
+            callback = cb)
+        return sum(saved_values.saveval)
+    end,
+    p)[1]))
